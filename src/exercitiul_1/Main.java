dd967cbe89d2a8d4b7c47800f104b5330a012d32
mariauliniuc <126767354+mariauliniuc@users.noreply.github.com>
2023-04-03 13:09:01 +0300

    Update Main.java

--- src/exercitiul_1/Main.java
+++ src/exercitiul_1/Main.java
@@ -1,4 +1,14 @@
 package exercitiul_1;
-
+import java.util.Scanner;
 public class Main {
+  public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Introduceti latimea: ");
+        float latime=scanner.nextFloat();
+        System.out.println("Introduceti lungimea: ");
+        float lungime=scanner.nextFloat();
+        float perimetru=2*(lungime+latime);
+        float arie=lungime*latime;
+        System.out.println("Aria este: "+arie);
+        System.out.println("Perimetrul este"+perimetru);
 }
